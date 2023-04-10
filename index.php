981dd167f0a7c30d97ecda4a2cfeefc45d3a5174
Serafin Piña <arespin23@gmail.com>
2023-04-10 13:52:12 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include_once "./uis/head.php" ?>
+    <link rel="stylesheet" href="./styles/index_style.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <nav>
+        <span id="open-close"><i class='bx bx-menu'></i></span>
+    </nav>
+    <aside>
+        <div class="container-svg">
+            <div>
+                <i class='bx bxs-home'></i>
+                <span>Inicio</span>
+            </div>
+        </div>
+        <div class="container-svg">
+
+        </div>
+            <div><i class='bx bx-layer'></i>
+            <span>Inventario</span>
+                
+        </div>
+        <div class="container-svg">
+            <div>
+                <i class='bx bxs-book'></i>
+                <span>Salida</span>
+            </div>
+        </div>
+        <div class="container-svg">
+            <div>
+                <i class='bx bx-layer'></i>
+                <span>Historial</span>
+            </div>
+
+        </div>
+        <div class="container-svg">
+            <div>
+                <i class='bx bxs-book'></i>
+                <span>Registrar BD Remota</span>
+            </div>
+            <div>
+                <i class='bx bxs-book'></i>
+                <span>Actualizar webs</span>
+            </div>
+            <div>
+                <i class='bx bxs-book'></i>
+                <span>Exportar</span>
+            </div>
+            <div>
+                <i class='bx bxs-book'></i>
+                <span>Importar</span>
+            </div>
+            <div>
+                <i class='bx bxs-book'></i>
+                <span>Actualizar a3erp</span>
+            </div>
+        </div>
+    </aside>
+
+    <script src="./scripts/scripts.js"></script>
+</body>
+
+</html>
