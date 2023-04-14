45ced8bdd522081c864efec2e09945b2a3c1f222
Serafin Piña <arespin23@gmail.com>
2023-04-14 08:49:08 +0200

    hover menu items

--- index.php
+++ index.php
@@ -18,39 +18,47 @@
         </div>
         <div class="options">
             <div>
-                <i class='bx bxs-home'></i>
+                <i class='bx bxs-home' onmouseover="over(this)" onmouseleave="leave(this)"></i>
                 <span class="option">Inicio</span>
-                <span class="span-i" >Inicio</span>
+                <dialog open class="">Inicio</dialog>
+                <!-- <span class="span-i" >Inicio</span> -->
             </div>
 
             <div>
-                <i class='bx bx-layer'></i>
-                <span class="option">Inventario</span>                
+                <i class='bx bx-layer' onmouseover="over(this)" onmouseleave="leave(this)"></i>
+                <span class="option">Inventario</span> 
+                <dialog open class="">Inventario</dialog>               
             </div>
 
             <div>
-                <i class='bx bxs-book'></i>
+                <i class='bx bxs-book'  onmouseover="over(this)" onmouseleave="leave(this)"></i>
                 <span class="option">Salida</span>
+                <dialog open class="">Salida</dialog> 
             </div>
             <div>
-                <i class='bx bx-layer'></i>
+                <i class='bx bx-layer'  onmouseover="over(this)" onmouseleave="leave(this)"></i>
                 <span class="option">Historial</span>
+                <dialog open class="">Historial</dialog> 
             </div>
             <div>
-                <i class='bx bxs-book'></i>
+                <i class='bx bxs-book'  onmouseover="over(this)" onmouseleave="leave(this)"></i>
                 <span class="option">Registrar BD Remota</span>
+                <dialog open class="">Registrar BD Remota</dialog> 
             </div>
             <div>
-                <i class='bx bxs-book'></i>
+                <i class='bx bxs-book'  onmouseover="over(this)" onmouseleave="leave(this)"></i>
                 <span class="option">Actualizar webs</span>
+                <dialog open class="">Actualizar webs</dialog> 
             </div>
             <div>
-                <i class='bx bxs-book'></i>
+                <i class='bx bxs-book'  onmouseover="over(this)" onmouseleave="leave(this)"></i>
                 <span class="option">Exportar</span>
+                <dialog open class="">Exportar</dialog> 
             </div>
             <div>
-                <i class='bx bxs-book'></i>
+                <i class='bx bxs-book'  onmouseover="over(this)" onmouseleave="leave(this)"></i>
                 <span class="option">Actualizar a3erp</span>
+                <dialog open class="">Actualizar a3erp</dialog> 
             </div>
             
         </div>
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,13 +4,37 @@ const aside = document.querySelector(".aside"),
 
 menu.onclick = ()=> aside.classList.toggle('active')
 
-ioption.forEach(element => {
-    element.addEventListener('mouseover', (e) =>  {
-        if(document.querySelector('.active')){
-            e.target.nextElementSibling.nextElementSibling.style.display = 'inline'
-            //console.log(e.target.nextElementSibling.nextElementSibling)
-        }else{
-            e.target.nextElementSibling.nextElementSibling.style.display = 'none'
-        }
-    })
-});
+// ioption.forEach(element => {
+//     element.addEventListener('mouseover', (e) =>  {
+//         if(document.querySelector('.active')){
+//             console.log(e.target.nextElementSibling.nextElementSibling)
+//             //console.log(e.target.nextElementSibling.nextElementSibling)
+//         }
+//     })
+// });
+
+function over(e) {
+    if(document.querySelector('.active')){
+        let dialogo = e.nextElementSibling.nextElementSibling
+        dialogo.classList.add('dialogo')
+        dialogo.style.display = "inline"
+    //     let span = document.createElement('span')
+    //     span.className = "span-i"
+    //     span.innerHTML= "EEE"
+    //     e.target.appendChild(span)
+    }
+}
+
+function leave(e) {
+    if(document.querySelector('.active')){
+        let dialogo = e.nextElementSibling.nextElementSibling
+        dialogo.classList.remove('dialogo')
+        dialogo.style.display = "none"
+        
+        
+    //     let span = document.createElement('span')
+    //     span.className = "span-i"
+    //     span.innerHTML= "EEE"
+    //     e.target.appendChild(span)
+    }
+}
