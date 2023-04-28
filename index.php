096ab3644d37e423f6f3a1ecb6355d0faf747779
Serafin Piña <arespin23@gmail.com>
2023-04-28 13:43:56 +0200

    imagen centrada

--- index.php
+++ index.php
@@ -1,27 +1,133 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- FONT AWESOME -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+
     <link rel="stylesheet" href="./styles/styles.css">
+
+    <!-- BOOTSTRAP  -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
     <script src="./scripts/scripts.js" defer></script>
     <title>Document</title>
 </head>
+
 <body>
+    <img src="./images/gestion-almacenes.png" alt="">
     <header>
-        <div class="div-menu-icon">
-            <i class="fa-solid fa-bars"></i>
+        <div class="logo">
+            <span class="span_logo mt-2 mb-2">
+                <a class="" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
+                    <i class="fa-solid fa-bars hamburguer" style="color: #fff;"></i>
+                </a>
+            </span>
+            
+            <h2 class="logo-title mt-1 mb-1 ms-2">Gestión de Almacén</h2>
+            
         </div>
     </header>
-    <aside class="aside-menu-icon">
-       <div class="options">
-            <div class="option">
-                <i class="fa-solid fa-book"></i>
-            </div>
-       </div>
 
+    <aside class="aside-menu-icon">
+        <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasExampleLabel">MENÚ</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+
+            <div class="offcanvas-body">
+
+                <span><i class="fa fa-dashboard me-2" style="color: #5f9ea0;"></i>Inventario</span>
+                <ul class="">
+                    <li class="" aria-disabled="true"><i class="fa-regular fa-circle me-2" style="color: #5f9ea0;"></i>Registrar</li>
+                    <li class="" aria-disabled="true"><i class="fa-regular fa-circle me-2" style="color: #5f9ea0;"></i>Buscar</li>
+
+                </ul>
+
+                <span><i class="fa-solid fa-right-from-bracket me-2" style="color: #5f9ea0;"></i>Salida</span>
+                <ul></ul>
+
+                <span><i class="fa-solid fa-clock-rotate-left me-2" style="color: #5f9ea0;"></i>Historial</span>
+                <ul class="">
+                    <li class="" aria-disabled="true"><i class="fa-regular fa-circle me-2" style="color: #5f9ea0;"></i>Entrada</li>
+                    <li class="" aria-disabled="true"><i class="fa-regular fa-circle me-2" style="color: #5f9ea0;"></i>Salida</li>
+
+                </ul>
+
+                <span><i class="fa-solid fa-book me-2" style="color: #5f9ea0;"></i>Registrar BD Remota</span>
+                <ul></ul>
+                <span><i class="fa-solid fa-book me-2" style="color: #5f9ea0;"></i>Actualizar Webs</span>
+                <ul></ul>
+                <span><i class="fa-solid fa-book me-2" style="color: #5f9ea0;"></i>Exportar</span>
+                <ul></ul>
+                <span><i class="fa-solid fa-book me-2" style="color: #5f9ea0;"></i>Importar</span>
+                <ul></ul>
+                <span><i class="fa-solid fa-book me-2" style="color: #5f9ea0;"></i>Actualizar a3erp</span>
+                <ul></ul>
+
+            </div>
+        </div>
     </aside>
+
+    <main class="mt-5 div">
+        
+        
+       
+        <section class="seccion_principal">
+            ddd
+        </section>
+        <section class="seccion_principal">
+            ddd
+        </section>
+        <section class="seccion_principal">
+            ddd
+        </section>
+        <section class="seccion_principal">
+            ddd
+        </section>
+        <section class="seccion_principal">
+            ddd
+        </section>
+        <section class="seccion_principal">
+            ddd
+        </section>
+        <section class="seccion_principal">
+            ddd
+        </section>
+        <section class="seccion_principal">
+            ddd
+        </section>
+        <section class="seccion_principal">
+            ddd
+        </section>
+        <section class="seccion_principal">
+            ddd
+        </section>
+        <section class="seccion_principal">
+            ddd
+        </section>
+        <section class="seccion_principal">
+            ddd
+        </section>
+        <section class="seccion_principal">
+            ddd
+        </section>
+        <section class="seccion_principal">
+            ddd
+        </section>
+        <section class="seccion_principal">
+            ddd
+        </section>
+       
+    </main>
+
 </body>
+
 </html>
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,26 @@
-let iMenu = document.querySelector('.aside-menu-icon >  .option')
+// let body = document.body
+// let iMenu = document.querySelector('#menu-icon')
+// let aside = document.querySelector('.aside-menu-icon')
+// let opciones = document.querySelectorAll('.imenu')
+// let menuSpan = document.querySelectorAll('.link_externo')
 
-iMenu.addEventListener('click', ()=>{
-    console.log("ww")
-})
+
+
+// menuSpan.forEach(span =>{
+//     span.addEventListener('click', (e)=>{
+//         e.preventDefault()
+//         e.target.classList.toggle('active')
+//         e.target.nextElementSibling.classList.toggle('show')
+//     })
+// })
+
+
+// iMenu.addEventListener('click', (e)=>{
+    
+//     e.target.classList.toggle('fa-times')
+//     e.target.classList.toggle('xmenu')
+    
+//     console.log(aside)
+//     aside.classList.toggle('show_aside')
+    
+// })
