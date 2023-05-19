5fd8129f50d8ab0312fdaa22a53c258e35e3e0c5
Serafin Piña <arespin23@gmail.com>
2023-05-19 13:45:54 +0200

    menu modularizado

---
+++ uis/menu-lateral.php
@@ -0,0 +1,43 @@
+ <aside class="aside-menu-icon">
+     <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+
+         <div class="offcanvas-header">
+             <h5 class="offcanvas-title" id="offcanvasExampleLabel">MENÚ</h5>
+             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+         </div>
+
+         <div class="offcanvas-body ">
+
+             <span><i class="fa fa-dashboard me-2" style="color: #fff;"></i>Inventario</span>
+             <ul class="">
+                 <li class="" aria-disabled="true"><i class="fa-regular fa-circle me-2" style="color: #fff;"></i>Registrar</li>
+                 <li class="" aria-disabled="true"><i class="fa-regular fa-circle me-2" style="color: #fff;"></i>Buscar</li>
+
+             </ul>
+
+             <span><i class="fa-solid fa-right-from-bracket me-2" style="color: #fff;"></i>Salida</span>
+             <ul></ul>
+
+             <span><i class="fa-solid fa-clock-rotate-left me-2" style="color: #fff;"></i>Historial</span>
+             <ul class="">
+                 <li class="" aria-disabled="true"><i class="fa-regular fa-circle me-2" style="color: #fff;"></i>Entrada</li>
+                 <li class="" aria-disabled="true"><i class="fa-regular fa-circle me-2" style="color: #fff;"></i>Salida</li>
+
+             </ul>
+
+             <span><i class="fa-solid fa-book me-2" style="color: #fff;"></i>Registrar BD Remota</span>
+             <ul></ul>
+             <span><i class="fa-solid fa-book me-2" style="color: #fff;"></i>Actualizar Webs</span>
+             <ul></ul>
+             <span><i class="fa-solid fa-book me-2" style="color: #fff;"></i>Exportar</span>
+             <ul></ul>
+             <span><i class="fa-solid fa-book me-2" style="color: #fff;"></i>Importar</span>
+             <ul></ul>
+             <span><i class="fa-solid fa-book me-2" style="color: #fff;"></i>Actualizar a3erp</span>
+             <ul></ul>
+
+
+
+         </div>
+     </div>
+ </aside>
--- views/vista.php
+++ views/vista.php
@@ -1,39 +1,9 @@
-<!DOCTYPE html>
-<html lang="es">
-
-<?php 
-    include_once './uis/head.php';
- ?>
-
-<body>
-    <img id="almacen" src="./images/gestion-almacenes.png" alt="">
-    <header>
-        <div class="logo">
-            <span class="span_logo mt-2 mb-2">
-                <a class="" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
-                    <i class="fa-solid fa-bars hamburguer" style="color: #fff;"></i>
-                </a>
-            </span>
-
-            <h2 class="logo-title mt-1 mb-1 ms-2">Gestión de Almacén</h2>
-
-        </div>
-
-    </header>
-
-    <?php 
-        include_once './uis/menu-lateral.php';
-    ?>
-
-    <main class="mt-5 div">
+<main class="mt-5 div">
 
         <div>
             <img id="imagen_pollo" src="./images/logo.png" alt="" srcset="">
         </div>
 
-        <!-- <section class="seccion_principal">
-            ddd
-        </section>
         <section class="seccion_principal">
             ddd
         </section>
@@ -165,15 +135,10 @@
         </section>
         <section class="seccion_principal">
             ddd
-        </section> -->
+        </section>
+        <section class="seccion_principal">
+            ddd
+        </section> 
 
 
     </main>
-    
-    <footer>
-        PIE
-    </footer>
-
-</body>
-
-</html>
